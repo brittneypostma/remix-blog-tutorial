1d617572da1492c32b53467195ef2fabd987e7cc
brittneypostma <bdesignedwebdev@gmail.com>
2022-08-01 11:07:36 -0400

    init

---
+++ app/models/post.server.ts
@@ -0,0 +1,31 @@
+import { prisma } from "~/db.server";
+
+export type { Post } from "@prisma/client";
+
+export async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string | undefined) {
+  return prisma.post.findUnique({
+    where: { slug },
+  });
+}
+
+// type Post = {
+//   slug: string;
+//   title: string;
+// };
+
+// export async function getPosts(): Promise<Array<Post>> {
+//   return [
+//     {
+//       slug: "my-first-post",
+//       title: "My First Post",
+//     },
+//     {
+//       slug: "90s-mixtape",
+//       title: "A Mixtape I Made Just For You",
+//     },
+//   ];
+// }
---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,28 @@
+import { marked } from "marked";
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getPost } from "~/models/post.server";
+import type { Post } from "~/models/post.server";
+import invariant from "tiny-invariant";
+type LoaderData = { post: Post; html: string };
+
+export const loader = async ({ params }: LoaderArgs) => {
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  const html = marked(post.markdown);
+  return json<LoaderData>({ post, html });
+};
+
+export default function PostSlug() {
+  const { post, html } = useLoaderData();
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="border-b-1 my-6 text-center text-3xl">{post.title}</h1>
+      <section dangerouslySetInnerHTML={{ __html: html }} />
+    </main>
+  );
+}
